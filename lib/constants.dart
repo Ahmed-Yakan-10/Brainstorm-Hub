4d3676998371f36ab3fedeac257da6a68b7661bc
Ahmed-Yakan-10 <salemdeff@gmail.com>
2024-07-23 06:22:46 +0300

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kFont = 'ConcertOne';
+
+const kIcon = 'assets/icons/brainstorming.png';
+
+const kPrimaryColor = Color(0xff2E3D7C);
+
+const kSecondaryColor = Color(0xffFFD80D);
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+
+import 'package:brainstorm_hub/constants.dart';
+import 'package:brainstorm_hub/views/notes_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BrainStormHub());
+}
+
+class BrainStormHub extends StatelessWidget {
+  const BrainStormHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark,fontFamily: kFont,),
+      home: const NotesView(),
+    );
+  }
+}
