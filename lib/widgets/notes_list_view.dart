99eb76752cf3637fbf090778997f05cf6c0c6df3
Ahmed-Yakan-10 <salemdeff@gmail.com>
2024-07-28 18:29:58 +0300

    display notes part one

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -1,24 +1,29 @@
+import 'package:brainstorm_hub/cubits/notes_cubit/notes_cubit.dart';
+import 'package:brainstorm_hub/models/note_model.dart';
 import 'package:brainstorm_hub/widgets/custom_note_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
 
-
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(vertical: 8),
-            child:  NoteItem(),
-          );
-        },
-      ),
-    );
+    return BlocBuilder<NotesCubit, NotesState>(builder: (context, state) {
+      List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes ?? [];
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        child: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemCount: notes.length,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8),
+              child: NoteItem(),
+            );
+          },
+        ),
+      );
+    });
   }
 }
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,14 +1,27 @@
 import 'package:brainstorm_hub/constants.dart';
+import 'package:brainstorm_hub/cubits/notes_cubit/notes_cubit.dart';
 import 'package:brainstorm_hub/widgets/custom_app_bar.dart';
 import 'package:brainstorm_hub/widgets/custom_note_item.dart';
 import 'package:brainstorm_hub/widgets/custom_search_icon.dart';
 import 'package:brainstorm_hub/widgets/notes_list_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class NotesViewBody extends StatelessWidget {
+class NotesViewBody extends StatefulWidget {
   const NotesViewBody({super.key});
+
+  @override
+  State<NotesViewBody> createState() => _NotesViewBodyState();
+}
+
+class _NotesViewBodyState extends State<NotesViewBody> {
+
+  void initState(){
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
