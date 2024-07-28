a263610820ab85aabc8707e5724a79de086ca1bd
Ahmed-Yakan-10 <salemdeff@gmail.com>
2024-07-28 08:17:36 +0300

    add simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:brainstorm_hub/constants.dart';
 import 'package:brainstorm_hub/cubits/add_note_cubit/add_notes_cubit.dart';
 import 'package:brainstorm_hub/models/note_model.dart';
+import 'package:brainstorm_hub/simple_bloc_observer.dart';
 import 'package:brainstorm_hub/views/notes_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
+
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
