1b6f2ecc83e5a547a5664f29ddbe658f93b3e8fb
Ahmed-Yakan-10 <salemdeff@gmail.com>
2024-07-28 07:58:50 +0300

    provide Add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:brainstorm_hub/constants.dart';
+import 'package:brainstorm_hub/cubits/add_note_cubit/add_notes_cubit.dart';
 import 'package:brainstorm_hub/models/note_model.dart';
 import 'package:brainstorm_hub/views/notes_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
@@ -17,13 +19,18 @@ class BrainStormHub extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: kFont,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context)=> AddNotesCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: kFont,
+        ),
+        home: const NotesView(),
       ),
-      home: const NotesView(),
     );
   }
 }
